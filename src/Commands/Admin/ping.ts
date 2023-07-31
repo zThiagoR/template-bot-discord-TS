db0b662e16a2a2fe0394c471093807b87ea64771
zThiagoR <thi.magalha13@gmail.com>
2023-07-30 23:01:39 -0300

    refactor: remove typing and export BaseCommand

--- src/Commands/Admin/ping.ts
+++ src/Commands/Admin/ping.ts
@@ -1,19 +1,18 @@
-import { ApplicationCommandDataResolvable, ApplicationCommandOptionType, Client, CommandInteraction } from 'discord.js';
+import { ChatInputCommandInteraction, Client, CommandInteraction } from 'discord.js';
+import BaseCommand from '../BaseCommands';
 
-export default class PingCommand {
-  data: ApplicationCommandDataResolvable;
-  cooldown?: number;
-  ephemeral?: boolean;
-  defer: boolean;
+export default class PingCommand extends BaseCommand {
 
   constructor() {
+    super();
+
     this.data = {
       name: 'ping',
       description: 'teste'
     }
   }
 
-  execute(client: Client<true>, int: CommandInteraction) {
+  execute(client: Client<true>, int: ChatInputCommandInteraction) {
     
   }
 }
